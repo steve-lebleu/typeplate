1903aeb3997d9ef97242d5a8331f51ef633acaf4
Steve <info@konfer.be>
2021-03-03 16:42:32 +0100

    ESLint compliance

--- src/api/validations/auth.validation.ts
+++ src/api/validations/auth.validation.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
 import * as Joi from 'joi';
 
 // POST api/v1/auth/register
--- src/api/validations/media.validation.ts
+++ src/api/validations/media.validation.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
 import * as Joi from 'joi';
 import * as schemas from '@validations/schemas';
 import { MIME_TYPE } from '@enums/mime-type.enum';

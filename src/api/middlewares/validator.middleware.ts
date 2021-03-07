5582a0293614ad4e982928776ed6e3a0c28d6dbc
Steve <info@konfer.be>
2021-03-07 22:40:41 +0100

    Simple & consistent config

--- src/api/middlewares/validator.middleware.ts
+++ src/api/middlewares/validator.middleware.ts
@@ -1,4 +1,4 @@
-import { ValidationConfiguration } from '@config/validation.config';
+import * as ExpressValidation from 'express-validation';
 
 /**
  * ExpressValidation wrapper as configured middleware
@@ -8,13 +8,33 @@ export class Validator {
   /**
    * @description Validation configuration instance
    */
-  private static configuration = new ValidationConfiguration();
+  private static instance: ExpressValidation = null;
+
+  /**
+  * @description Default options
+  */
+  private static options = Object.freeze({
+    allowUnknownBody: true,
+    allowUnknownHeaders: false,
+    allowUnknownQuery: false,
+    allowUnknownParams: false,
+    allowUnknownCookies: false,
+    status: 400,
+    statusText: 'Bad Request',
+    contextRequest: false
+  });
 
   /**
    * @description ExpressValidation instance
    * @alias ExpressValidation
    */
-  static validate = Validator.configuration.handler;
+  static get validate(): ExpressValidation {
+    if (!Validator.instance) {
+      Validator.instance = ExpressValidation;
+      Validator.instance.options(Validator.options);
+    }
+    return Validator.instance;
+  }
 
   constructor() {}
 }

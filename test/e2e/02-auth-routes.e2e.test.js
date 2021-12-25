77796bb68f7f60a26ef3549e40a7a10840dc1254
Steve <info@konfer.be>
2021-12-25 11:11:38 +0100

    Test full suite

--- test/e2e/02-auth-routes.e2e.test.js
+++ test/e2e/02-auth-routes.e2e.test.js
@@ -23,7 +23,7 @@ const { encrypt } = require(process.cwd() + '/dist/api/core/utils/string.util');
 const { user } = require(process.cwd() + '/test/utils/fixtures');
 const { doRequest, doQueryRequest } = require(process.cwd() + '/test/utils');
 
-describe.only('Authentification routes', function () {
+describe('Authentification routes', function () {
   
   let agent, _token, _loggedAccessToken, _loggedRefreshToken, _refreshToken, _user, _apikey;
   

86ddd2efe00b19521c3c41dc5e0aa5ce1ce1c65f
Steve <info@konfer.be>
2024-02-16 09:02:48 +0100

    fix: datasource for CLI

--- src/api/config/datasource.config.ts
+++ src/api/config/datasource.config.ts
@@ -1,24 +1,29 @@
 import 'reflect-metadata';
 
-import { DataSource, MixedList } from 'typeorm';
-import { TYPEORM } from '@config/environment.config';
+import { DataSource } from 'typeorm';
 
 /**
- * Typeorm default configuration
+ * Typeorm default configuration used by CLI. This one is the new ormconfig.json
  *
  * @see https://http://typeorm.io
  */
-export default new DataSource({
-  type: TYPEORM?.TYPE as 'mariadb' | 'mysql',
-  name: TYPEORM.NAME,
-  host: TYPEORM.HOST,
-  port: TYPEORM.PORT,
-  username: TYPEORM.USER,
-  password: TYPEORM.PWD,
-  database: TYPEORM.DB,
-  entities: TYPEORM.ENTITIES as unknown as MixedList<string>,
-  subscribers: TYPEORM.SUBSCRIBERS as unknown as MixedList<string>,
-  synchronize: TYPEORM.SYNC,
-  logging: TYPEORM.LOG,
-  cache: TYPEORM.CACHE
+export const dataSource = new DataSource({
+  type: 'mysql',
+  name: 'default',
+  host: process.env.CI_DB_HOST || 'localhost',
+  port: 3306,
+  username: process.env.CI_DB_USER || 'root',
+  password: process.env.CI_DB_PWD || 'passw0rd',
+  database: process.env.CI_DB_NAME || 'typeplate_test',
+  entities: [
+    'dist/api/core/models/**/*.model.js',
+    'dist/api/resources/**/*.model.js'
+  ],
+  subscribers: [
+    'dist/api/core/subscribers/**/*.subscriber.js',
+    'dist/api/resources/**/*.subscriber.js'
+  ],
+  synchronize: false,
+  logging: false,
+  cache: false
 });
